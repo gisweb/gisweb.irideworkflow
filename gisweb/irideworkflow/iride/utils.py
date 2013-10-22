99b50048948750f850bed7ce2c5f126ccf2e94e9
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-10-22 12:50:06 +0200

    fixed bug

--- gisweb/irideworkflow/iride/utils.py
+++ gisweb/irideworkflow/iride/utils.py
@@ -6,20 +6,21 @@ import os
 path = 'src/gisweb.irideworkflow/gisweb/irideworkflow/iride/'
 filepath = 'utils/dbo_GEO_COMUNE.csv'
 
-with open(path+filepath, 'r') as source:
-    # Codici Comuni Iride
-    CCI =  csv_to_array(source.read(), delimiter=';', quotechar='"')
-
 def getCodcom(codfisco):
     """
     dato un codice fiscale di un comune voglio il corrispondente codice ISTAT.
     """
-    assert codfisco, 'Variable has to be evaluated as True'
-    i = CCI[0].index('GCO_ISTAT')
-    flt = lambda row : row[4].upper()==codfisco.upper()
-    res = filter(flt, CCI)
-    vals = tuple(set([j[i] for j in res]))
-    assert len(vals)==1, 'Too many values obtained from file %s.' % filepath
-    return int(vals[0])
+
+    with open(path+filepath, 'r') as source:
+        # Codici Comuni Iride
+        CCI =  csv_to_array(source.read(), delimiter=';', quotechar='"')
+
+        assert codfisco, 'Variable has to be evaluated as True'
+        i = CCI[0].index('GCO_ISTAT')
+        flt = lambda row : row[4].upper()==codfisco.upper()
+        res = filter(flt, CCI)
+        vals = tuple(set([j[i] for j in res]))
+        assert len(vals)==1, 'Too many values obtained from file %s.' % filepath
+        return int(vals[0])
 
     
