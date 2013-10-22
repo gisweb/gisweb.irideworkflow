d23e899fbe87fe2ce2f8ef1826cf7579376379e0
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-10-22 15:01:45 +0200

    fixed relative file path

--- gisweb/irideworkflow/iride/utils.py
+++ gisweb/irideworkflow/iride/utils.py
@@ -4,23 +4,28 @@ from Products.CMFPlomino.PlominoUtils import csv_to_array
 import os
 
 path = 'src/gisweb.irideworkflow/gisweb/irideworkflow/iride/'
+# in case spervisord is used
+if os.getcwd().endswith('bin'):
+    path = '../' + path
 filepath = 'utils/dbo_GEO_COMUNE.csv'
 
+def getcci():
+    with open(path+filepath, 'r') as source:
+        # Codici Comuni Iride
+        CCI =  csv_to_array(source.read(), delimiter=';', quotechar='"')
+    return CCI
+    
 def getCodcom(codfisco):
     """
     dato un codice fiscale di un comune voglio il corrispondente codice ISTAT.
     """
-
-    with open(path+filepath, 'r') as source:
-        # Codici Comuni Iride
-        CCI =  csv_to_array(source.read(), delimiter=';', quotechar='"')
-
-        assert codfisco, 'Variable has to be evaluated as True'
-        i = CCI[0].index('GCO_ISTAT')
-        flt = lambda row : row[4].upper()==codfisco.upper()
-        res = filter(flt, CCI)
-        vals = tuple(set([j[i] for j in res]))
-        assert len(vals)==1, 'Too many values obtained from file %s.' % filepath
-        return int(vals[0])
+    assert codfisco, 'Variable has to be evaluated as True'
+    CCI = getcci()
+    i = CCI[0].index('GCO_ISTAT')
+    flt = lambda row : row[4].upper()==codfisco.upper()
+    res = filter(flt, CCI)
+    vals = tuple(set([j[i] for j in res]))
+    assert len(vals)==1, 'Too many values obtained from file %s.' % filepath
+    return int(vals[0])
 
     
