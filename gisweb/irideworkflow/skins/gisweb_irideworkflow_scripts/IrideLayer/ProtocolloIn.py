8ff3790a08a990bd46b3b62abf52aada195144f8
Manuele Pesenti <manuele.pesenti@gmail.com>
2014-02-26 14:01:34 +0100

    ProtocolloIn ora cerca prima attributi locali in resources e aggiornamento concessione 11 e 12

--- gisweb/irideworkflow/skins/gisweb_irideworkflow_scripts/IrideLayer/ProtocolloIn.py
+++ gisweb/irideworkflow/skins/gisweb_irideworkflow_scripts/IrideLayer/ProtocolloIn.py
@@ -14,16 +14,29 @@ Ricava dal PlominoDocument le informazioni utili per costruire un oggetto
 ProtocolloIn-like.
 """
 
-plominoDocument = context.getParentDocument()
-conf_name = '%s.txt' % plominoDocument.getForm().getFormName()
+# Example code:
 
-msg = 'Layer per la pratica "%s" NON ancora implementato!' % plominoDocument.Title().decode(
-    'ascii', errors='replace').encode('ascii', errors='replace')
-assert (conf_name in context.config.keys()), msg
+"""
+Ricava dal PlominoDocument le informazioni utili per costruire un oggetto
+ProtocolloIn-like.
+"""
 
 from gisweb.irideworkflow import conf2dict
 
-conf = conf2dict(str(getattr(script.config, conf_name)))
+plominoDocument = context.getParentDocument()
+conf_name = '%s.txt' % plominoDocument.getForm().getFormName()
+resources = plominoDocument.getParentDatabase().resources
+
+msg = 'Layer per la pratica "%s" NON ancora implementato!' % plominoDocument.Title().decode(
+    'ascii', errors='replace').encode('ascii', errors='replace')
+
+if 'IrideLayer' in resources.keys() and conf_name in resources.IrideLayer.keys():
+    conf = conf2dict(str(getattr(resources.IrideLayer, conf_name)))
+elif conf_name in script.config.keys():
+    conf = conf2dict(str(getattr(script.config, conf_name)))
+else:
+    assert False, msg
+
 defaults = conf2dict(str(getattr(script.config, 'defaults.txt')))['CONFIG']
 
 return dict(
