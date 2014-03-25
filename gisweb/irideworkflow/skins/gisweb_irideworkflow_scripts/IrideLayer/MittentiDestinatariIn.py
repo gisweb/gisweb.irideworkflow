27524be1ca84e47f82379028fb57c1e26a51a3a1
Manuele Pesenti <manuele.pesenti@gmail.com>
2014-03-25 15:10:24 +0100

    debug per invio cointestatari

--- gisweb/irideworkflow/skins/gisweb_irideworkflow_scripts/IrideLayer/MittentiDestinatariIn.py
+++ gisweb/irideworkflow/skins/gisweb_irideworkflow_scripts/IrideLayer/MittentiDestinatariIn.py
@@ -72,29 +72,28 @@ cointestatario = dict(
 def get_cointestatario(rec):
     form = plominoDocument.getForm()
     fld = form.getFormField('elenco_cointestatari')
-    gridfields = fld.field_mapping.split(',')
+    gridfields = fld.getSettings().field_mapping.split(',')
     nfo = dict(zip(gridfields, rec))
 
     cointestatario = dict(
         CodiceFiscale = lambda doc: doc['fisica_cf_cointestatari'],
-        CognomeNome = lambda doc: '%s %s' % (doc['fisica_cognome_cointestatari'], doc['fisica_nome_cointestatari'], ),
-        Indirizzo = lambda doc: '%s, %s' % (doc['fisica_indirizzo_cointestatari'], doc['fisica_civico_cointestatari'], ),
-        CodiceComuneResidenza = lambda doc: doc['fisica_cod_cat_cointestatari'],
-        DataNascita = lambda doc: doc['fisica_data_nato_cointestatari'],
-        CodiceComuneNascita = lambda doc: doc['fisica_cod_cat_nato_cointestatari'],
-        Nazionalita = lambda doc: 'IT' if doc['fisica_cod_cat_nato_cointestatari'] != 'EE' else doc['fisica_comune_nato_cointestatari'],
+        CognomeNome = lambda doc: '%s %s' % (doc['fisica_cognome'], doc['fisica_nome'], ),
+        Indirizzo = lambda doc: '%s, %s' % (doc['fisica_indirizzo'], doc['fisica_civico'], ),
+        CodiceComuneResidenza = lambda doc: doc['fisica_cod_cat'],
+        DataNascita = lambda doc: doc['fisica_data_nato'],
+        CodiceComuneNascita = lambda doc: doc['fisica_cod_cat_nato'],
+        Nazionalita = lambda doc: 'IT' if doc['fisica_cod_cat_nato'] != 'EE' else doc['fisica_comune_nato'],
         TipoSogg = lambda doc: 'S',
         TipoPersona = lambda doc: 'FI',
     )
 
-    return dict([(k,f(rec)) for k,f in cointestatario.items()])
-    
+    return dict([(k,f(nfo)) for k,f in cointestatario.items()])
 
 for el in (PersonaFisica, PersonaGiuridica, ):
     MittentiDestinatari.append(dict([(k,f(plominoDocument)) for k,f in el.items()]))
 
-if 'sub_fisica_cointestatari' in plominoDocument.getForm().getSubforms():
-    MittentiDestinatari.append(dict([(k,f(plominoDocument)) for k,f in cointestatario.items()]))
+#if 'sub_fisica_cointestatari' in plominoDocument.getForm().getSubforms():
+    #MittentiDestinatari.append(dict([(k,f(plominoDocument)) for k,f in cointestatario.items()]))
 
 if plominoDocument.hasItem('elenco_cointestatari'):
     cointestatari = plominoDocument.getItem('elenco_cointestatari', []) or []
