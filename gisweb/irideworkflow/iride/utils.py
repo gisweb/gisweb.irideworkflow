4c3be25a4bdf354352c2950f3db63316ba9f78dd
Manuele Pesenti <manuele.pesenti@gmail.com>
2014-03-27 14:11:17 +0100

    getCodcom accetta anche codfisco nullo e restituisce None

--- gisweb/irideworkflow/iride/utils.py
+++ gisweb/irideworkflow/iride/utils.py
@@ -9,16 +9,16 @@ def getCodcom(codfisco, error=False):
     """
     dato un codice fiscale di un comune voglio il corrispondente codice ISTAT.
     """
-    assert codfisco, 'Variable has to be evaluated as True'
+
+    if codfisco in (None, '', ): return None
+
     i = CCI[0].index('GCO_ISTAT')
     j = CCI[0].index('GCO_CODCAT')
     flt = lambda row : row[j].upper()==codfisco.upper()
     res = filter(flt, CCI)
-    
+
     vals = tuple(set([j[i] for j in res]))
     if error:
         assert len(vals)==1, 'CODCAT: %s corresponds too may values: %s' % (codfisco, vals)
     # in case of multiple results I get the last one (hoping it's an updated version)
     return int(vals[0])
-
-    
